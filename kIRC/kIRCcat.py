783ecf95a37e739ee352ed6307b04b1945df43fa
jskmr <ojaskumar2@gmail.com>
2016-07-14 12:34:16 +0530

    *Removed config.py *Fixed indentation *Made kIRCcat.py python 3 compatible

--- kIRC/kIRCcat.py
+++ kIRC/kIRCcat.py
@@ -17,118 +17,118 @@ from threading import Thread
 Builder.load_file("kvirc.kv")
 
 class ChatScreen(Screen):
-	pass
+    pass
  
 class chatMsg(GridLayout):
-	pass
+    pass
 
 class chatTab(TabbedPanelItem):
-	chatView = ObjectProperty(None)
-	
-	def __init__(self,title,**kwargs):
-		super(chatTab, self).__init__(**kwargs)
-		self.chatView.bind(minimum_height=self.chatView.setter('height'))
-		self.text = title
-		self.c = None
-		
-	def triggerMsg(self):
-		e = Thread(target=self.c.send_it,args=())
-		e.start()
+    chatView = ObjectProperty(None)
+    
+    def __init__(self,title,**kwargs):
+        super(chatTab, self).__init__(**kwargs)
+        self.chatView.bind(minimum_height=self.chatView.setter('height'))
+        self.text = title
+        self.c = None
+        
+    def triggerMsg(self):
+        e = Thread(target=self.c.send_it,args=())
+        e.start()
 
 class IRCCat(irc.client.SimpleIRCClient):
-	def __init__(self, target, gui):
-		irc.client.SimpleIRCClient.__init__(self)
-		self.target = target
-		self.gui = gui
+    def __init__(self, target, gui):
+        irc.client.SimpleIRCClient.__init__(self)
+        self.target = target
+        self.gui = gui
 
-	def on_welcome(self, connection, event):
-		if irc.client.is_channel(self.target):
-			connection.join(self.target)
-		else:
-			pass
+    def on_welcome(self, connection, event):
+        if irc.client.is_channel(self.target):
+            connection.join(self.target)
+        else:
+            pass
 
-	def on_disconnect(self, connection, event):
-		self.connection.quit("Using irc.client.py")
+    def on_disconnect(self, connection, event):
+        self.connection.quit("Using irc.client.py")
 
-	def on_pubmsg(self, connection, event):
-		msgBox = chatMsg()
-		msgBox.ids.senderLbl.text = event.source
-		#msgBox.ids.timeLbl
-		msgBox.ids.msgLbl.text = event.arguments[0]
-		self.gui.ids.chatView.add_widget(msgBox)
-		print event
-	
-	def on_privmsg(self, connection, event):
-		msgBox = chatMsg()
-		msgBox.ids.senderLbl.text = event.source
-		#msgBox.ids.timeLbl
-		msgBox.ids.msgLbl.text = event.arguments[0]
-		self.gui.ids.chatView.add_widget(msgBox)
-		print event
+    def on_pubmsg(self, connection, event):
+        msgBox = chatMsg()
+        msgBox.ids.senderLbl.text = event
+        #msgBox.ids.timeLbl
+        msgBox.ids.msgLbl.text = event.arguments[0]
+        self.gui.ids.chatView.add_widget(msgBox)
+        print(event)
+    
+    def on_privmsg(self, connection, event):
+        msgBox = chatMsg()
+        msgBox.ids.senderLbl.text = event.source
+        #msgBox.ids.timeLbl
+        msgBox.ids.msgLbl.text = event.arguments[0]
+        self.gui.ids.chatView.add_widget(msgBox)
+        print(event)
 
-	def send_it(self):
-		while 1:
-			line = self.gui.ids.msgInp.text
-			if line == '':
-				break
-			self.connection.privmsg(self.target, line)
-			break
-	   
+    def send_it(self):
+        while 1:
+            line = self.gui.ids.msgInp.text
+            if line == '':
+                break
+            self.connection.privmsg(self.target, line)
+            break
+       
 class IrcApp(App):
-	sm = ScreenManager()
-	cons = []
-	def build(self):
-		self.chatScrn = ChatScreen()
-		self.sm.add_widget(self.chatScrn)
-		return self.sm
+    sm = ScreenManager()
+    cons = []
+    def build(self):
+        self.chatScrn = ChatScreen()
+        self.sm.add_widget(self.chatScrn)
+        return self.sm
 
-	def startChat(self):
-		server = self.chatScrn.ids.serverInp.text
-		try: 
-			if port != '':
-				port = int(self.chatScrn.ids.portInp.text)
-		except:
-			port = 6667
+    def startChat(self):
+        server = self.chatScrn.ids.serverInp.text
+        try: 
+            if port != '':
+                port = int(self.chatScrn.ids.portInp.text)
+        except:
+            port = 6667
 
-		nick = self.chatScrn.ids.nickInp.text
-		target = self.chatScrn.ids.targetInp.text
-		
-		iCons = 0
-		try:
-			if self.cons:
-				for nets in self.cons:
-					if nets.connection.server == server and nets.connection.nickname == nick:
-						nets.connection.join(target) #need to join with an object that already has the con
-						ircTab = chatTab(target)		
-						ircTab.c = nets
-						self.chatScrn.ids.UItabs.add_widget(ircTab)
-						break
-					elif nets < len(self.cons):
-						iCons += 1
-						print iCons
-						continue
-					else:
-						ircTab = chatTab(target)
-						con = IRCCat(target,ircTab)
-						con.connect(server, port, nick)
-						self.cons.append(con)	
-						print self.cons	
-						ircTab.c = con
-						self.chatScrn.ids.UItabs.add_widget(ircTab)
-						Thread(target=con.start,args=()).start()
-						break
-			else:
-				ircTab = chatTab(target)		
-				con = IRCCat(target,ircTab)
-				con.connect(server, port, nick)
-				self.cons.append(con)	
-				print self.cons	
-				ircTab.c = con
-				self.chatScrn.ids.UItabs.add_widget(ircTab)
-				Thread(target=con.start,args=()).start()						
-		except irc.client.ServerConnectionError as x:
-			print(x)
-			sys.exit(0)		
+        nick = self.chatScrn.ids.nickInp.text
+        target = self.chatScrn.ids.targetInp.text
+        
+        iCons = 0
+        try:
+            if self.cons:
+                for nets in self.cons:
+                    if nets.connection.server == server and nets.connection.nickname == nick:
+                        nets.connection.join(target) #need to join with an object that already has the con
+                        ircTab = chatTab(target)        
+                        ircTab.c = nets
+                        self.chatScrn.ids.UItabs.add_widget(ircTab)
+                        break
+                    elif nets < len(self.cons):
+                        iCons += 1
+                        print(iCons)
+                        continue
+                    else:
+                        ircTab = chatTab(target)
+                        con = IRCCat(target,ircTab)
+                        con.connect(server, port, nick)
+                        self.cons.append(con)    
+                        print(self.cons)    
+                        ircTab.c = con
+                        self.chatScrn.ids.UItabs.add_widget(ircTab)
+                        Thread(target=con.start,args=()).start()
+                        break
+            else:
+                ircTab = chatTab(target)        
+                con = IRCCat(target,ircTab)
+                con.connect(server, port, nick)
+                self.cons.append(con)    
+                print(self.cons)    
+                ircTab.c = con
+                self.chatScrn.ids.UItabs.add_widget(ircTab)
+                Thread(target=con.start,args=()).start()                        
+        except irc.client.ServerConnectionError as x:
+            print(x)
+            sys.exit(0)        
 
-		
+        
 IrcApp().run()
--- profile.py
+++ profile.py
@@ -3,64 +3,64 @@ import json
 SAVE_PATH = 'data/profiles.json'
 
 class Network(object):
-	def __init__(self, name, address, port, login_command=None):
-		self.name = name
-		self.address = address
-		self.port = port
-		self.login_command = login_command
+    def __init__(self, name, address, port, login_command=None):
+        self.name = name
+        self.address = address
+        self.port = port
+        self.login_command = login_command
 
-	def __repr__(self):
-		return self.name
+    def __repr__(self):
+        return self.name
 
 class Profile(object):
-	def __init__(self, name, description, nicks, networks):
-		self.name = name
-		self.description = description
-		self.nicks = nicks
-		self.networks = networks
+    def __init__(self, name, description, nicks, networks):
+        self.name = name
+        self.description = description
+        self.nicks = nicks
+        self.networks = networks
 
-	def __repr__(self):
-		return self.name
+    def __repr__(self):
+        return self.name
 
 class ProfileHandler(object):
-	def __init__(self, save_path=SAVE_PATH):
-		self.save_path = save_path
-		self.profiles = []
+    def __init__(self, save_path=SAVE_PATH):
+        self.save_path = save_path
+        self.profiles = []
 
-		with open(self.save_path, 'r') as profiles_file:
-			profiles_data = json.loads(profiles_file.read())
+        with open(self.save_path, 'r') as profiles_file:
+            profiles_data = json.loads(profiles_file.read())
 
-		for name in profiles_data.keys():
-			data = profiles_data[name]
-			self.new(name, data['description'], data['nicks'],
-				[Network(net, *data['networks'][net]) for net in data['networks'].keys()])
+        for name in profiles_data.keys():
+            data = profiles_data[name]
+            self.new(name, data['description'], data['nicks'],
+                [Network(net, *data['networks'][net]) for net in data['networks'].keys()])
 
-	def save(self):
-		profiles_dict = {}
+    def save(self):
+        profiles_dict = {}
 
-		for profile in self.all():
-			profiles_dict[profile.name] = {
-				'description' : profile.description,
-				'networks' : {},
-				'nicks' : profile.nicks
-			}
+        for profile in self.all():
+            profiles_dict[profile.name] = {
+                'description' : profile.description,
+                'networks' : {},
+                'nicks' : profile.nicks
+            }
 
-			for net in profile.networks:
-				profiles_dict[profile.name]['networks'][net.name] = [
-				net.address, net.port, net.login_command]
+            for net in profile.networks:
+                profiles_dict[profile.name]['networks'][net.name] = [
+                net.address, net.port, net.login_command]
 
-		with open(self.save_path, 'w') as profiles_file:
-			profiles_json = json.dumps(profiles_dict, indent=4)
-			profiles_file.write(profiles_json)			
+        with open(self.save_path, 'w') as profiles_file:
+            profiles_json = json.dumps(profiles_dict, indent=4)
+            profiles_file.write(profiles_json)            
 
-	def new(self, name, description, nicks, networks):
-		self.profiles.append(Profile(name, description, nicks, networks))
+    def new(self, name, description, nicks, networks):
+        self.profiles.append(Profile(name, description, nicks, networks))
 
-	def get(self, name):
-		for profile in self.profiles:
-			if profile.name == name:
-				return profile
-		return None
+    def get(self, name):
+        for profile in self.profiles:
+            if profile.name == name:
+                return profile
+        return None
 
-	def all(self):
-		return self.profiles
+    def all(self):
+        return self.profiles
